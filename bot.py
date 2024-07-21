e0c780a15eebef70a8dfa306e2d3761a4b58a1a5
armiro <arman.hgh.1374@gmail.com>
2024-07-21 15:48:53 -0400

    insert env variables into the functions

--- bot.py
+++ bot.py
@@ -12,18 +12,16 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 load_dotenv('./variables.env')
 
-BOT_TOKEN = os.getenv(key='BOT_TOKEN')
-APP_LINK = os.getenv(key='APP_LINK')
-
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    app_link = os.getenv(key='APP_LINK')
     guid = update.effective_user.id
     user = users_collection.find_one({'guid': guid})
     if not user:
         users_collection.insert_one({'guid': guid, 'balance': 0})
 
     welcome_msg = "Welcome to the mini-game crypto bot! Tap on funny Donald Trump to earn $DJT tokens!"
-    keyboard = [[InlineKeyboardButton(text='Launch the Game!', web_app=WebAppInfo(url=APP_LINK))]]
+    keyboard = [[InlineKeyboardButton(text='Launch the Game!', web_app=WebAppInfo(url=app_link))]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text(welcome_msg, reply_markup=reply_markup, parse_mode='HTML')
 
@@ -32,7 +30,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 def main() -> None:
-    app = Application.builder().token(BOT_TOKEN).build()
+    bot_token = os.getenv(key='BOT_TOKEN')
+    app = Application.builder().token(bot_token).build()
     app.add_handler(CommandHandler('start', start))
     app.run_polling()
 
