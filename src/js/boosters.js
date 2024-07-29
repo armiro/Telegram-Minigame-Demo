daeccbb223e80cbbac539293337256dcd46bab4e
armiro <arman.hgh.1374@gmail.com>
2024-07-29 10:37:24 -0400

    implement booster status memory

--- src/js/boosters.js
+++ src/js/boosters.js
@@ -1,28 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
     const boosterContainers = document.querySelectorAll('.booster-container');
+    const remainingBalance = document.querySelector('.balance-display .user-balance');
     const userID = window.localStorage.getItem('userID');
     let totalCoins = parseInt(window.localStorage.getItem('totalCoins'), 10);
-    console.log('total user coins:', totalCoins);
+    remainingBalance.textContent = totalCoins.toString();
 
     boosterContainers.forEach(container => {
         const boosterCostElement = container.querySelector('.booster-cost');
+        const boosterID = boosterCostElement.id;
         const boosterCost = parseInt(boosterCostElement.textContent, 10);
+        const boosterStatus = window.localStorage.getItem(`booster-${boosterID}-status`);
+
+        if (boosterStatus === 'done') {
+            boosterCostElement.textContent = 'done';
+        }
+
         boosterCostElement.addEventListener('click', () => {
-            if (totalCoins > boosterCost) {
-                // update totalCoins value
+            if (boosterCostElement.textContent !== 'done' && totalCoins >= boosterCost) {
+                // update totalCoins value & remaining balance
                 totalCoins -= boosterCost;
-                // replace boosterCost value with 'done'
+                remainingBalance.textContent = totalCoins.toString();
+                // update totalCoins value in local storage (required when buying multiple boosters)
+                window.localStorage.setItem('totalCoins', totalCoins);
+                // replace boosterCost value with 'done' (when booster is only available once)
                 boosterCostElement.textContent = 'done';
+                window.localStorage.setItem(`booster-${boosterID}-status`, 'done')
                 // update user balance on the server
                 fetch(`${BASE_URL}/tap`, {
                     method: 'POST',
                     headers: {'Content-Type': 'application/x-www-form-urlencoded'},
                     body: new URLSearchParams({guid: userID, balance: totalCoins.toString()})
                 })
-            } else {
+            } else if (boosterCostElement.textContent !== 'done'){
                 alert('Not enough coins!');
             }
         })
     })
-
 })
