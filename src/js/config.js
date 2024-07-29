4f1696cc959246a1f934b02e532b7c83cec01ae0
armiro <arman.hgh.1374@gmail.com>
2024-07-29 08:58:16 -0400

    store constants in confij file

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+// define global constants
+const BASE_URL = 'https://pheasant-creative-goldfish.ngrok-free.app';
+const TAP_LIMIT_MAX = 100;
+const TAP_INCREMENT_INTERVAL = 5000;
+const BALANCE_UPDATE_DELAY = 1000;
