d61114172bc0073f1aa8a6873846025123495a85
armiro <arman.hgh.1374@gmail.com>
2024-07-30 05:47:44 -0400

    add speed feature

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ CORS(app, resources={r"/*": {"origins": "*"}})  # enable cross-origin resource s
 @app.route('/tap', methods=['POST'])
 def tap():
     user_id = request.form['guid']
-    new_balance = int(request.form['balance'])
+    new_balance = request.form['balance']
+    speed = request.form['speed']
     user = users_collection.find_one({'guid': user_id})
     if user:
-        users_collection.update_one({'guid': user_id}, {'$set': {'balance': new_balance}})
+        users_collection.update_one({'guid': user_id}, {'$set': {'balance': new_balance, 'speed': speed}})
         return jsonify({})
     else:
         return jsonify({'error': 'User not found!'}), 404
@@ -24,9 +25,9 @@ def get_balance():
     user_id = request.form.get('guid')
     user = users_collection.find_one({'guid': user_id})
     if user:
-        return jsonify({'balance': user['balance'], 'ref_code': user['ref_code']})
+        return jsonify({'balance': user['balance'], 'ref_code': user['ref_code'], 'speed': user['speed']})
     else:
-        users_collection.insert_one({'guid': user_id, 'balance': 0})
+        users_collection.insert_one({'guid': user_id, 'balance': 0, 'speed': 1})
         return jsonify({'balance': 0})
 
 
