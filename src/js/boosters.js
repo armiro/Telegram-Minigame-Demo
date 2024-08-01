2ce3f711af64c6593cd38c8b29e9acb3ace7354d
armiro <arman.hgh.1374@gmail.com>
2024-08-01 13:33:18 -0400

    update session storage, and remove server call

--- src/js/boosters.js
+++ src/js/boosters.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const boosterContainers = document.querySelectorAll('.booster-container');
     const remainingBalance = document.querySelector('.balance-display .user-balance');
-    const userID = window.sessionStorage.getItem('userID');  // better to use tgWebApp attr?
     let totalCoins = parseInt(window.sessionStorage.getItem('totalCoins'), 10);
     remainingBalance.textContent = totalCoins.toString();
 
@@ -19,18 +18,11 @@ document.addEventListener('DOMContentLoaded', () => {
             if (boosterCostElement.textContent !== 'max' && totalCoins >= boosterCost) {
                 // update totalCoins value & remaining balance
                 totalCoins -= boosterCost;
+                window.sessionStorage.setItem('totalCoins', totalCoins);
                 remainingBalance.textContent = totalCoins.toString();
-                // update totalCoins value in local storage (required when buying multiple boosters) redundant?
-                // window.sessionStorage.setItem('totalCoins', totalCoins);
                 // replace boosterCost value with 'max' (no higher speeds available)
                 boosterCostElement.textContent = 'max';
                 window.sessionStorage.setItem(`booster-${boosterID}-status`, 'max')
-                // update user balance & speed on the server
-                fetch(`${BASE_URL}/tap`, {
-                    method: 'POST',
-                    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-                    body: new URLSearchParams({guid: userID, balance: totalCoins.toString(), speed: '2.0'})
-                })
             } else if (boosterCostElement.textContent !== 'max'){
                 alert('Not enough coins!');
             }
