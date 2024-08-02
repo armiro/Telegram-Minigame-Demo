4f439413843759c4a6ba7d6b0a164b95801d5b96
armiro <arman.hgh.1374@gmail.com>
2024-08-02 14:27:15 -0400

    remove url, set referralCode as session variable

--- src/js/referral.js
+++ src/js/referral.js
@@ -1,11 +1,10 @@
 function shareReferral() {
     const shareText = `Join me on playing Mustachio and receive 200 coins as your welcome bonus!\n` +
-        `https://t.me/mustachio_bot?start=${window.localStorage.getItem('referralCode')}`;
+        `https://t.me/mustachio_bot?start=${window.sessionStorage.getItem('referralCode')}`;
     if (navigator.share) {
         navigator.share({
             title: 'Mustachio Referral',
-            text: shareText,
-            // url: window.location.href // url of current page
+            text: shareText
         })
             .then(() => console.log('ref link was successfully shared!'))
             .catch((error) => console.error('Error sharing:', error))
