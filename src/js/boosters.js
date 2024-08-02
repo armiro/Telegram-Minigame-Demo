e466d583d4e98b3b626acbed04e825f6fa5c33ac
armiro <arman.hgh.1374@gmail.com>
2024-08-02 17:53:10 -0400

    import api call and update balance on the server

--- src/js/boosters.js
+++ src/js/boosters.js
@@ -1,6 +1,9 @@
+import { updateBalance } from "./utils.js";
+
 document.addEventListener('DOMContentLoaded', () => {
     const boosterContainers = document.querySelectorAll('.booster-container');
     const remainingBalance = document.querySelector('.balance-display .user-balance');
+    const userID = window.sessionStorage.getItem('userID');
     let totalCoins = parseInt(window.sessionStorage.getItem('totalCoins'), 10);
     remainingBalance.textContent = totalCoins.toString();
 
@@ -8,21 +11,23 @@ document.addEventListener('DOMContentLoaded', () => {
         const boosterCostElement = container.querySelector('.booster-cost');
         const boosterID = boosterCostElement.id;
         const boosterCost = parseInt(boosterCostElement.textContent, 10);
-        const boosterStatus = window.localStorage.getItem(`booster-${boosterID}-status`);
+        const boosterStatus = window.sessionStorage.getItem(`booster-${boosterID}-status`);
 
         if (boosterStatus === 'max') {
             boosterCostElement.textContent = 'max';
         }
 
-        boosterCostElement.addEventListener('click', () => {
+        boosterCostElement.addEventListener('click', async () => {
             if (boosterCostElement.textContent !== 'max' && totalCoins >= boosterCost) {
+                // update coin balance on the server-side
+                await updateBalance(BASE_URL, userID, totalCoins, 2);
                 // update totalCoins value & remaining balance
                 totalCoins -= boosterCost;
                 window.sessionStorage.setItem('totalCoins', totalCoins);
                 remainingBalance.textContent = totalCoins.toString();
                 // replace boosterCost value with 'max' (no higher speeds available)
                 boosterCostElement.textContent = 'max';
-                window.localStorage.setItem(`booster-${boosterID}-status`, 'max')
+                window.sessionStorage.setItem(`booster-${boosterID}-status`, 'max')
             } else if (boosterCostElement.textContent !== 'max'){
                 alert('Not enough coins!');
             }
